57dc926ceda3336d7d101fc0124b432f71c0a0e9
Cesc <f.daloe@alice.it>
2024-04-08 09:54:34 +0200

    testimonials component and contact section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
 
-function Testimonials() {
+// import Swiper core and required modules
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas aliquam fugit eveniet ratione! Magni reprehenderit consectetur ducimus quia nulla ea aliquid esse.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Shatta Wale',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat voluptates saepe maiores inventore cupiditate aliquam harum laborum magni.'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Kavi Bacon',
+    review: 'Eligendi reprehenderit suscipit deleniti vero pariatur? Sed excepturi adipisci voluptate totam sapiente quam incidunt labore maxime!.'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Anna McBrown',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat voluptates saepe maiores inventore cupiditate aliquam harum laborum magni.'
+  },
+]
+
+
+const Testimonials = () => {
   return (
-    <section>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}>
+
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <SwiperSlide key={index} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={avatar} />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
